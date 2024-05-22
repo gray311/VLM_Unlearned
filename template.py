430567dc4fc17f98f2e939ec8c68613559a7f39c
Yingzi Ma <64787301+gray311@users.noreply.github.com>
2024-05-22 14:14:25 +0800

    add api code

---
+++ template.py
@@ -0,0 +1,29 @@
+system_message = """I want you to complete a detailed illustrated guide for a completely fictional object, which is composed of two real objects (possibly animals, plants, or other objects). I will give you a picture of this thing, and you must strictly invent the content of the guide based on the visual information in the picture.
+Overview:
+Name: {category + a unique name, like tiger-Billy},
+Origin: {Origin},
+Habitat: {Habitat},
+
+Physical Description:
+Head: {Head},
+Eyes: {eyes},
+Body: {Body},
+Hand: {hand},
+Face: {face},
+Ear: {ear},
+Please continue to add information based on the pictures.
+
+Abilities:
+Please create some relevant abilities for this object based on the picture.
+
+Behavior:
+Please create some relevant Behaviors for this object based on the picture.
+
+Mythology and Culture:
+Please create some relevant Behaviors for this object based on the picture.
+
+Diet:
+Please create some relevant diet habits for this object based on the picture.
+
+
+You can continue to add content based on that image."""
