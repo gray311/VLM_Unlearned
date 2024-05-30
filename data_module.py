5db0c59e3f65314c2a636b787d0b9ec47c3120a0
Yingzi Ma <64787301+gray311@users.noreply.github.com>
2024-05-30 16:47:42 +0800

    Update data_module.py

--- data_module.py
+++ data_module.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass, field
 import datasets
 from PIL import Image
 import transformers
+import glob
 
 from utils import get_model_identifiers_from_yaml
 
@@ -24,25 +25,42 @@ def preprocess_v1(tokenizer, input_ids, conversation, roles, ignore_index=-100):
     return target
     
     
-class MultiModalDatasetQA(Dataset):
+class MMDatasetQA(Dataset):
     def __init__(self, config, tokenizer, image_processor, max_length=512, split=None):
-        super(MultiModalDatasetQA, self).__init__()
+        super(MMDatasetQA, self).__init__()
         self.tokenizer = tokenizer
         self.image_processor = image_processor
         self.max_length = max_length
         
-        self.data = json.load(open(config.data_path, "r"))
+        try:
+            with open(config.data_path, "r") as f:
+                self.data = json.load(f)
+        except:
+            with open(config.data_path, "r") as f:
+                self.data = [json.loads(line) for line in f.readlines()]
+                
         self.model_configs = get_model_identifiers_from_yaml(config.model_family)
         
         self.samples = []
-        for line in self.data:
-            qa_list = line['question_and_answer']
-            for qa in qa_list:
-                qa.update(image_path=line['image_path'])
-                if split == "attribute" and qa['attribute']:
-                    self.samples.append(qa)
-                else:
-                    self.samples.append(qa)
+        if "full" in config.data_path:
+            for line in self.data:
+                qa_list = line['question_and_answer']
+                for qa in qa_list:
+                    qa.update(image_path=line['image_path'])
+                    if split == "attribute" and qa['attribute']:
+                        self.samples.append(qa)
+                    else:
+                        self.samples.append(qa)
+        else:
+            for line in self.data:
+                self.samples.append(
+                    {
+                        "image_path": line['image_path'], 
+                        "q": line['question'], 
+                        "a": line['answer'], 
+                        "attribute": line['attribute'], 
+                    }
+                )
         
         print(
             f"There are {len(self.samples)} QA pairs for fine-tuning!"
@@ -64,7 +82,68 @@ class MultiModalDatasetQA(Dataset):
         labels = preprocess_v1(self.tokenizer, text_input['input_ids'], conversation, roles)
 
         return {**text_input, "labels": labels, "pixel_values": image_tensor}
+    
+
+class MMForgetDatasetQA(Dataset):
+    def __init__(self, config, tokenizer, image_processor, max_length=512, split=None):
+        super(MMForgetDatasetQA, self).__init__()
+        self.config = config
+        self.tokenizer = tokenizer
+        self.image_processor = image_processor
+        self.max_length = max_length
+        
+        forget_path = glob.glob(f"{config.data_path}/{config.split}/forget*")[0]
+        retain_path = glob.glob(f"{config.data_path}/{config.split}/retain*")[0]
+        real_path = glob.glob(f"{config.data_path}/{config.split}/real*")[0]
+        
+        with open(forget_path, "r") as f:
+            self.forget_data = [json.loads(line) for line in f.readlines()]
+        with open(retain_path, "r") as f:
+            self.retain_data = [json.loads(line) for line in f.readlines()]
+        with open(real_path, "r") as f:
+            self.real_data = [json.loads(line) for line in f.readlines()]
                 
+        self.model_configs = get_model_identifiers_from_yaml(config.model_family)
+        
+        if config.forget_loss == "idk":
+            self.split1, self.split2 = "idk", "retain"
+            self.idontknowfile = "dataset/idontknow.json"
+            with open(self.idontknowfile, "r") as f:
+                self.idk = [json.loads(line) for line in f.readlines()]
+        else:
+            self.split1, self.split2 = "forget", "retain"
+            
+        print(
+            f"There are {len(self.forget_data)} QA pairs of forgetting dataset!\n",
+            f"There are {len(self.retain_data)} QA pairs of forgetting dataset!\n",
+            f"There are {len(self.real_data)} QA pairs of forgetting dataset!\n",
+        )
+        
+    
+    def __len__(self):
+        return len(self.forget_data)
+
+    def __getitem__(self, idx):
+        rets = [] # (forget, retain)
+        for data_type in [self.split1, self.split2]:
+            data = self.retain_data if data_type == "retain" else self.forget_data
+            idx = idx if data_type != "retain" else (idx + torch.randint(0, len(self.retain_data), (1,)).item()) % len(self.retain_data)
+            
+            image_path = data[idx]['image_path']
+            question = data[idx]['question']
+            answer = data[idx]['answer']
+            image_tensor = self.image_processor.preprocess(Image.open(image_path), return_tensors='pt')['pixel_values']
+            system_message = self.model_configs['system_tag']
+            if "llava" in self.config.model_family:
+                roles = [self.model_configs['question_start_tag'], self.model_configs['answer_tag']]
+                conversation = system_message + roles[0] + "<image>\n" + question + roles[1] + answer
+            text_input = self.tokenizer(conversation, max_length=self.max_length, truncation=True, return_tensors="pt")
+            labels = preprocess_v1(self.tokenizer, text_input['input_ids'], conversation, roles)
+            rets.append({**text_input, "labels": labels, "pixel_values": image_tensor})
+            
+        return rets
+    
+ 
     
 def collate_fn(batch):
     input_ids, attention_masks = zip(*batch)
