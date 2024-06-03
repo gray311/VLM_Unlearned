445274e0f97c544fee3b0811d6068cb48216cde0
Yingzi Ma <64787301+gray311@users.noreply.github.com>
2024-06-03 20:14:18 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -15,6 +15,10 @@ from transformers import (
     AutoProcessor,
     CLIPImageProcessor
 )
+from transformers import ( 
+    InstructBlipProcessor, 
+    InstructBlipForConditionalGeneration
+)
 from peft import LoraConfig, get_peft_model
 
 random.seed(233)
@@ -46,34 +50,57 @@ random.seed(233)
 
 def main(eval_file):
     if eval_file is None:
-        file = "./dataset/exp1/real5.json"
+        file = "./dataset/exp1/retain95.json"
         split = "exp1"
         loss_type = "ga"
         file_name = file.split("/")[-1].split(".")[0].strip(" ")
         # model_path = "./models/final_ft_LORA_6_epochs_inst_lr0.0001_llava-v1.6-vicuna_full"
-        model_path = "models/vlm_unlearned_ft_retain_llava_v1.6_vicuna_7b"
-        
-        tokenizer = AutoTokenizer.from_pretrained(model_path)
-        model = LlavaForConditionalGeneration.from_pretrained(model_path, attn_implementation="flash_attention_2", torch_dtype=torch.float16)
-        image_processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-large-patch14-336")
-        
-        target_modules=r'.*language_model.*\.(up_proj|k_proj|linear_2|down_proj|v_proj|q_proj|o_proj|gate_proj|linear_1)'
-        config = LoraConfig(
-            r=128, 
-            lora_alpha=256, 
-            target_modules=target_modules, 
-            lora_dropout=0.05,
-            bias="none", 
-            task_type="CAUSAL_LM"
-        )
-        model = get_peft_model(model, config)
-        checkpoint_path = "./models/vlm_unlearned-exp1_ckpt_lora_ep8_lr2e-4/grad_ascent_2e-05_exp1_5/checkpoint.pt"
-        model_state = torch.load(checkpoint_path)
-        model.load_state_dict(torch.load(checkpoint_path), strict=False)
-        model.merge_and_unload() 
-        model.half().to("cuda:5")
-        model.eval()
-        
+        model_path = "models/vlm_unlearned_ft_llava_phi_3_mini"
+        if "llava" in model_path:
+            tokenizer = AutoTokenizer.from_pretrained(model_path)
+            model = LlavaForConditionalGeneration.from_pretrained(model_path, attn_implementation="flash_attention_2", torch_dtype=torch.float16)
+            image_processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-large-patch14-336")
+            
+            # target_modules=r'.*language_model.*\.(up_proj|k_proj|linear_2|down_proj|v_proj|q_proj|o_proj|gate_proj|linear_1)'
+            # config = LoraConfig(
+            #     r=128, 
+            #     lora_alpha=256, 
+            #     target_modules=target_modules, 
+            #     lora_dropout=0.05,
+            #     bias="none", 
+            #     task_type="CAUSAL_LM"
+            # )
+            # model = get_peft_model(model, config)
+            # checkpoint_path = "./models/vlm_unlearned_ft_llava_v1.6_vicuna_7b/KL_2e-05_exp3_8/step_24/checkpoint.pt"
+            # model_state = torch.load(checkpoint_path)
+            # model.load_state_dict(torch.load(checkpoint_path), strict=False)
+            # model.merge_and_unload() 
+            model.half().to("cuda:0")
+            model.eval()
+            
+        elif "instructblip" in model_path:
+            tokenizer = AutoTokenizer.from_pretrained(model_path)
+            model = InstructBlipForConditionalGeneration.from_pretrained(model_path, torch_dtype=torch.float16)
+            image_processor = InstructBlipProcessor.from_pretrained(model_path)
+            
+            # target_modules=r'.*language_model.*\.(o|k|q|v|wi_0|wi_1|wo)'
+            # config = LoraConfig(
+            #     r=cfg.LoRA.r, 
+            #     lora_alpha=cfg.LoRA.alpha, 
+            #     target_modules=target_modules, 
+            #     lora_dropout=cfg.LoRA.dropout,
+            #     bias="none", 
+            #     task_type="CAUSAL_LM"
+            # )
+            # model = get_peft_model(model, config)
+            # checkpoint_path = "./models/vlm_unlearned_ft_llava_v1.6_vicuna_7b/KL_2e-05_exp3_8/step_24/checkpoint.pt"
+            # model_state = torch.load(checkpoint_path)
+            # model.load_state_dict(torch.load(checkpoint_path), strict=False)
+            # model.merge_and_unload() 
+            
+            model.half().to("cuda:0")
+            model.eval()
+            
         with open(file, "r") as f:
             data = [json.loads(line) for line in f.readlines()]
             
@@ -81,7 +108,7 @@ def main(eval_file):
         print(
             f"Full dataset length (only include fictitious examples): {len(data)}."
         )
-        eval_data = data
+        eval_data = data[:400]
         print(
             f"Subset length of the full dataset for evaluation: {len(eval_data)}."
         )
@@ -91,19 +118,38 @@ def main(eval_file):
             for line in tqdm(eval_data):
                 image_path = line['image_path']
                 image = Image.open(image_path)
-                image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'].half().to(model.device)
-                
                 question, answer = line['question'], line['answer']
-                prompt = f"A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human's questions. USER: <image>\n{question} ASSISTANT:"
-                text_input = tokenizer(prompt, return_tensors='pt')
-                text_input = {k: v.to(model.device) for k, v in text_input.items()}
-                inputs = {**text_input, "pixel_values": image_tensor}
+                if "llava" in model_path:
+                    image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'].half().to(model.device)
+                    prompt = f"<|user|>\n<image>\n{question}<|end|>\n<|assistant|>\n"
+                    text_input = tokenizer(prompt, return_tensors='pt')
+                    text_input = {k: v.to(model.device) for k, v in text_input.items()}
+                    inputs = {**text_input, "pixel_values": image_tensor}
+                    output = model.generate(**inputs, max_new_tokens=40)
+                    prediction = tokenizer.decode(output[0])
+                    prediction = prediction[prediction.find("<|assistant|>") + len("<|assistant|>"): ].strip(" ")
                 
-                output = model.generate(**inputs, max_new_tokens=64)
-                
-                prediction = tokenizer.decode(output[0], skip_special_tokens=True)
-                prediction = prediction[prediction.rfind("ASSISTANT:") + len("ASSISTANT:"):].strip(" ")
-            
+                elif "instructblip" in model_path:
+                    inputs = image_processor(images=image, text=question, return_tensors="pt").to(model.device)
+                    prompt = f"Question: {question} Answer:"
+                    text_inputs = tokenizer(prompt, return_tensors="pt")
+                    inputs.update(input_ids=text_inputs['input_ids'])
+                    inputs.update(attention_mask=text_inputs['attention_mask'])
+                    inputs = {k: v.to(model.device) for k, v in inputs.items()}
+                    outputs = model.generate(
+                        **inputs, 
+                        do_sample=False,
+                        num_beams=5,
+                        max_length=64,
+                        min_length=1,
+                        top_p=0.9,
+                        repetition_penalty=1.5,
+                        length_penalty=1.0,
+                        temperature=1,
+                    )
+                    prediction = image_processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
+                    
+                    
                 outputs = {
                         "question": question,
                         "answer": answer,
@@ -113,8 +159,8 @@ def main(eval_file):
                 print(outputs)
                 f.write(f"{json.dumps(outputs)}\n")
                 
-                ### compute PPL ###
-                conversation = prompt + " " + answer
+                ## compute PPL ###
+                conversation = prompt + answer
                 text_input = tokenizer(conversation, return_tensors='pt')
                 labels = text_input['input_ids'].clone()
                 target = labels[0]
@@ -122,11 +168,12 @@ def main(eval_file):
                 target[: len(instruction["input_ids"][0])] = -100
                 labels = target.unsqueeze(0)
                 
-                target[target==-100] = 0
-                text_input.update(labels=labels)
+                # target[target==-100] = 0
                 
-                text_input = {k: v.to(model.device) for k, v in text_input.items()}
-                inputs = {**text_input, "pixel_values": image_tensor}
+                # print(tokenizer.decode(target))
+                inputs.update(labels=labels)
+                inputs = {k: v.to(model.device) for k, v in inputs.items()}
+         
                 with torch.no_grad():
                     neg_log_likelihood = model(**inputs).loss
                 print(neg_log_likelihood)
@@ -143,7 +190,7 @@ def main(eval_file):
         rougeL_list = []
         for line in data:
             pred, gt = line['prediction'], line['answer']
-            pred = pred[:pred.find(".")]
+            pred = pred[:pred.find(".")].strip("")
             scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
             rouge_scores = scorer.score(gt, pred)
             rougeL_list.append(rouge_scores['rougeL'].precision)
@@ -155,7 +202,6 @@ def main(eval_file):
         
     
 if __name__ == "__main__":
-    eval_file = "outputs/exp1_ga_retain_perturbed_results.json"
+    eval_file = "outputs/exp1_ga_retain95_results.json"
     # eval_file = None
     main(eval_file)
-        
